d89f665406bdb6584fdbd624567b6e090801aad5
Daniel <dnkelly97@gmail.com>
2019-12-04 22:32:10 -0600

    hex conversion function

--- FUK.py
+++ FUK.py
@@ -10,3 +10,8 @@ raw_hex = soup.get_text()
 txs = raw_hex[80:]
 print(txs[0:2])
 print(int(txs[0], 16))
+
+'''Returns the decimal conversion of the specified number of bytes of
+ hex data from the start bit (zero by default)'''
+def convert(hex_data, num_bytes, start_bit=0):
+    return int(hex_data[start_bit:num_bytes*2], 16)
