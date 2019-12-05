8a5bcd923b87a68d0179a66e0ba699d6fd020a03
Daniel <dnkelly97@gmail.com>
2019-12-04 22:21:15 -0600

    jus lil things

--- FUK.py
+++ FUK.py
@@ -1,12 +1,12 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 
-url = "https://blockchain.info/rawblock/0000000000000000000af1a5baef627412f1774dbf9f37ed7e16c9c923f66897?format=hex"
+url = "https://blockchain.info/rawblock/000000000019d6689c085ae165831e934ff763ae46a2a6c172b3f1b60a8ce26f?format=hex"
 html = urlopen(url)
 
 soup = BeautifulSoup(html, 'lxml')
 raw_hex = soup.get_text()
 
 txs = raw_hex[80:]
-print(raw_hex[0])
-print(int(raw_hex[0]))
+print(txs[0:2])
+print(int(txs[0], 16))
